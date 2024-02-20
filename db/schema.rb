560839aceb18f5751cf3a2a2dddeaa6431b0c24f
younes essetti <younes.essetti@gmail.com>
2024-02-20 13:54:03 +0100

    all controlers workin

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_20_104157) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_20_105335) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -26,4 +38,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_20_104157) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,42 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.destroy_all
+Task.destroy_all
+
+#Create a user
+User.create!(
+  first_name: "Younes",
+  last_name: "ST",
+  email: "younes@lewagon.com",
+  password: "123456"
+)
+
+#Create 5 tasks without faker
+Task.create!(
+  title: "Buy milk",
+  details: "Go to the supermarket and buy milk",
+  completed: false,
+  user: User.first
+)
+
+Task.create!(
+  title: "Buy bread",
+  details: "Go to the bakery and buy bread",
+  completed: false,
+  user: User.first
+)
+
+Task.create!(
+  title: "Buy eggs",
+  details: "Go to the supermarket and buy eggs",
+  completed: false,
+  user: User.first
+)
+
+Task.create!(
+  title: "Buy cheese",
+  details: "Go to the supermarket and buy cheese",
+  completed: false,
+  user: User.first
+)
